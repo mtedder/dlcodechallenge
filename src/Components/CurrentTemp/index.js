a3a959bfe3748f7cf6480be539ca7c18679e7468
mtedder <maurice_tedder@hotmail.com>
2020-07-03 22:45:08 -0400

    Created component for current temp page. Remove default code and added custom code.

---
+++ src/Components/CurrentTemp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CurrentTemp() {
+    return (
+        <div className="current-temp">
+            <h2>Current Temperature</h2>
+        </div>
+    );
+}
+
+export default CurrentTemp;
